885d3e91f615ba103590fafa92cab3d58a35e6fb
sugyoung lee <tnrud4685@naver.com>
2022-07-20 21:49:03 +0900

    four commit

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,52 +1,24 @@
-let videos = [
-    {
-        title:"First Video",
-        rating:5,
-        comments:2,
-        createdAt: "2 minutes ago",
-        views:59,
-        id:1
-    },
-    {
-        title:"Second Video",
-        rating:5,
-        comments:2,
-        createdAt: "2 minutes ago",
-        views:1,
-        id:2
-    },
-    {
-        title:"Third Video",
-        rating:5,
-        comments:2,
-        createdAt: "2 minutes ago",
-        views:59,
-        id:3
-    },
-];
+import Video from "../models/Video";
 
-export const trending = (req, res) => {
-    return res.render("home", {pageTitle: "Home", videos});
+export const home = async (req, res) => {
+    const videos = await Video.find({});
+    return res.render("home", {pageTitle: "Home", videos });
 };
 
 export const watch = (req, res) => {
     const { id } = req.params;
     // ㄴ> == const id = req.params.id;
-
-    const video = videos[id-1];
-    return res.render("watch" , {pageTitle : `Watching ${video.title}`, video:video });
+    return res.render("watch" , {pageTitle : `Watching`});
 };
 
 export const getEdit = (req, res) => {
     const { id } = req.params;
-    const video = videos[id-1];
-    return res.render("edit", {pageTitle: `Editing: ${video.title}`, video:video});
+    return res.render("edit", {pageTitle: `Editing`});
 };
 
 export const postEdit = (req, res) => {
     const { id } = req.params;
     const { title } = req.body;
-    videos[id-1].title = title;
     return res.redirect(`/videos/${id}`);
 };
 
@@ -54,18 +26,22 @@ export const getUpload = (req, res) => {
     return res.render("upload", {pageTitle: "Upload Video"});
 }
 
-export const postUpload = (req, res) => {
-    const { title } = req.body;
-    const newVideo = {
-        title,
-        rating:0,
-        comments:0,
-        createdAt: "Just now",
-        views:0,
-        id: videos.length + 1
-    };
-    videos.push(newVideo); 
+export const postUpload = async (req, res) => {
+    const { title, description, hashtags } = req.body;
+    // db에 저장하는 방식 .create() or .save()
+    await Video.create({
+        // videoschema : req.body ,
+        title:title,
+        description:description,
+        createdAt: Date.now(),
+        hashtags:hashtags.split(",").map(word =>`#${word}`),
+        meta:{
+            views:0,
+            rating:0,
+        }
+        
+    });
     return res.redirect("/");
-}
+};
 
 
---
+++ src/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+// database
+mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
+    useNewUrlParser : true, 
+    useUnifiedTopology : true,
+});
+
+const db = mongoose.connection;
+
+// db error message
+const handleError = (error) => console.log("❌ DB Error", error);
+db.on("error", handleError);
+
+// db connection message
+const handleOpen = () => console.log("✅ Connected to DB");
+db.once("open", handleOpen);

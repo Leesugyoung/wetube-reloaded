c5f1e06291c8a48de3549d3e3a133eb294d72c1a
sugyoung lee <tnrud4685@naver.com>
2022-09-23 15:35:39 +0900

    fianl

--- src/init.js
+++ src/init.js
@@ -4,7 +4,7 @@ import "./models/Video";
 import "./models/User";
 import "./models/Comment";
 import app from "./server"
-
+ 
 const PORT = process.env.PORT || 4000;
 
 const handleListening = () => 

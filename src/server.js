5d8ae7edb76b348d2ddd96df06df325b307fdee5
sugyoung lee <tnrud4685@naver.com>
2022-09-25 19:09:37 +0900

    crossorigin push

--- src/server.js
+++ src/server.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
     res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
-    );
+    ),
     next();
 });
 app.use("/videos", videoRouter);

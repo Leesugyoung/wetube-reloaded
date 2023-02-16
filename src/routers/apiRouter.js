4b99b5b021cdf3c0db23a48470f0e60d68ab66c5
sugyoung lee <tnrud4685@naver.com>
2023-02-17 02:02:12 +0900

    video, comments Api routers

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -9,6 +9,5 @@ const apiRouter = express.Router();
 
 apiRouter.post("/videos/:id([0-9a-f]{24})/view", registerView);
 apiRouter.post("/videos/:id([0-9a-f]{24})/comment", creatComment);
-// ↓코드챌린지(댓글삭제)
 apiRouter.delete("/comments/:id([0-9a-f]{24})/delete", deleteComment);
 export default apiRouter;
